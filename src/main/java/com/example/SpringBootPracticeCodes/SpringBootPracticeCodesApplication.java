ba3303d8099289aa43931d2e8dcdb2842a30db85
shwetha-s-codes <shwethakodimar29@gmail.com>
2026-02-23 17:46:15 +0530

    Repo Created

---
+++ src/main/java/com/example/SpringBootPracticeCodes/SpringBootPracticeCodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPracticeCodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootPracticeCodes/SpringBootPracticeCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPracticeCodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
